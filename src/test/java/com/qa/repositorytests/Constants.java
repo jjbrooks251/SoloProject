c78ae7c3353499e6065c35d13891863742a9a239
jjbrooks251 <jjbrooks251@outlook.com>
2019-06-09 11:49:13 +0100

    blank MapRepo tests added

--- src/test/java/com/qa/repositorytests/Constants.java
+++ src/test/java/com/qa/repositorytests/Constants.java
@@ -2,8 +2,11 @@ package com.qa.repositorytests;
 
 public class Constants {
 	
-	public String user1 = "{\"id\":6,\"username\":\" jbro95\",\"password\":\"Jbroo95\",\"email\":\"jbro95@qa.com\"}";
-	public String user2 = "{\"id\":1,\"username\":\" jbro95\",\"password\":\"Jbroo95\",\"email\":\"jbro95@qa.com\"}";
+	public final String user1 = "{\"id\":6,\"username\":\" jbro95\",\"password\":\"Jbrooks95\",\"email\":\"jbro95@qa.com\"}";
+	public final String user2 = "{\"id\":1,\"username\":\" jbro95\",\"password\":\"Jbrooks95\",\"email\":\"jbro95@qa.com\"}";
+	public final String sameName = "{\"id\":7,\"username\":\" jbro95\",\"password\":\"Jbrooks95\",\"email\":\"jbrok95@qa.com\"}";
+	public final String sameEmail = "{\"id\":8,\"username\":\" jbrok95\",\"password\":\"Jbrooks95\",\"email\":\"jbro95@qa.com\"}";
+	public final String smallPass = "{\"id\":9,\"username\":\" jjbro95\",\"password\":\"Jbroo95\",\"email\":\"jjbro95@qa.com\"}";
 	
 	
 	
--- src/test/java/com/qa/repositorytests/Tests.java
+++ src/test/java/com/qa/repositorytests/Tests.java
@@ -1,5 +1,150 @@
 package com.qa.repositorytests;
 
-public class Tests {
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import com.qa.persistance.repository.UserMapRepository;
+import com.qa.util.JSONUtil;
+
+public class Tests {
+	
+	private JSONUtil jsonUtil;
+	private UserMapRepository amp;
+	
+	@Before
+	public void setup() {
+		jsonUtil = new JSONUtil();
+		amp = new UserMapRepository();
+
+	}
+
+	@Test
+	public void getAllUsers() {
+
+	}
+	
+	@Ignore
+	public void getAUser() {
+
+	}
+	
+	@Ignore
+	public void getUserNotExist() {
+
+	}
+	
+	@Ignore
+	public void getMultipleUsers() {
+
+	}
+	
+	@Ignore
+	public void createUser() {
+
+	}
+	
+	@Ignore
+	public void create2Users() {
+
+	}
+	
+	@Ignore
+	public void createUserSameId() {
+
+	}
+	
+	@Ignore
+	public void createUserSameName() {
+
+	}
+	
+	@Ignore
+	public void createUserSameEmail() {
+
+	}
+	
+	@Ignore
+	public void createMissingInfo() {
+
+	}
+	
+	@Ignore
+	public void createSmallPassword() {
+
+	}
+	
+	@Ignore
+	public void createPasswordNoNumber() {
+
+	}
+	
+	@Ignore
+	public void createPasswordNoUpper() {
+
+	}
+	
+	@Ignore
+	public void createPasswordNoLower() {
+
+	}
+	
+	@Ignore
+	public void deleteUser() {
+
+	}
+
+	@Ignore
+	public void deleteDoesntExist() {
+
+	}
+	
+	@Ignore
+	public void updateUser() {
+
+	}
+	
+	@Ignore
+	public void updateDoesntExist() {
+
+	}
+	
+	@Ignore
+	public void updateUserSameName() {
+
+	}
+	
+	@Ignore
+	public void updateUserSameEmail() {
+
+	}
+	
+	@Ignore
+	public void updateMissingInfo() {
+
+	}
+	
+	@Ignore
+	public void updateSmallPassword() {
+
+	}
+	
+	@Ignore
+	public void updatePasswordNoNumber() {
+
+	}
+	
+	@Ignore
+	public void updatePasswordNoUpper() {
+
+	}
+	
+	@Ignore
+	public void updatePasswordNoLower() {
+
+	}
+	
+	
 }
